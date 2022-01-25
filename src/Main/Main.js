82518b76066e9e31f93c6fe848e5873648486104
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-24 17:18:55 -0800

    passed animals array down to animalslist

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Main.css';
 import backgroundImg from '../images/background.png';
+import { animals } from './data.js';
+import AnimalList from '../AnimalList/AnimalList.js';
 
 export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-
+      <AnimalList animals={animals}/>
 
     </main>
   );
