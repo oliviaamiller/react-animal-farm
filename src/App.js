b4e431a877a8c2f6a34614bc1e8cf758261130fb
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-24 16:50:33 -0800

    imported animals array and passed as props to main

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from './Header/Header.js';
 import Footer from './Footer/Footer.js';
 import Main from './Main/Main.js';
+import { animals } from './data.js';
 
 function App() {
   return (
     <div className="App">
       <Header greeting='Welcome to the Farm!'/>
-      <Main />
+      <Main animals={animals}/>
       <Footer email='cuteanimals@farm.org'/>
 
     </div>
