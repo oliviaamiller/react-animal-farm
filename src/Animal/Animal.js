90df36528d089c1e157675c41b52f928462c8209
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-24 18:05:06 -0800

    writing rendering animals function

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function Animal(props) {
   return <div>
-    {props.name}
-    {props.type}
-    {props.says}
+    <img src={ `./animals/${props.type}.svg` } />
+    <p>{props.name}</p>
+    <p>{props.type}</p>
+    <p>{props.says}</p>
 
   </div>;
 }
