2007504199a03355783784a469c3bc50efe9fa20
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-25 17:26:19 -0800

    header and footer displaying properly

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Animal.css';
 
 export default function Animal({ name, type, says, left, top }) {
-  return <div className='animal' style={{ left: left, top: top }}>
+  return <div className='animal' style={{ left: left, top: top }} >
     <img alt={`${name}`} src={ `./animals/${type}.svg` } />
     <p className='animalName'>{name}</p>
     <p>{says}</p>
